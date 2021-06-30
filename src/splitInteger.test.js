fa3dfde48a759013e83def08ee2b6dc3f477e605
Aalderaan <chyregd@gmail.com>
2021-06-30 17:13:25 +0200

    Changes

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -3,7 +3,7 @@
 describe(`Function 'splitInteger':`, () => {
   const splitInteger = require('./splitInteger');
 
-  it(`function 'splitInteger' should be declared`, () => {
+  it(`should be declared`, () => {
     expect(splitInteger).toBeInstanceOf(Function);
   });
 
@@ -11,11 +11,12 @@ describe(`Function 'splitInteger':`, () => {
     expect(splitInteger()).toBeInstanceOf(Array);
   });
 
-  it(`variables should be of type 'number'`, () => {
+  it(`values should be of type 'number'`, () => {
     expect(splitInteger()).not.toBeNaN();
   });
 
-  it(`should return an array: [5, 5, 5, 5, 6, 6]`, () => {
+  it(`should return an array: [5, 5, 5, 5, 6, 6] if num  = 32, 
+  parts = 6`, () => {
     expect(splitInteger(32, 6))
       .toEqual(expect.arrayContaining([5, 5, 5, 5, 6, 6]));
   });
@@ -26,5 +27,9 @@ describe(`Function 'splitInteger':`, () => {
 
   it(`should return an array: [37, 37, 37] if num = 111, parts = 3`, () => {
     expect(splitInteger(111, 3)).toEqual(expect.arrayContaining([37, 37, 37]));
+  });
+
+  it(`should return an array with 'num' in ascending order`, () => {
+    expect(splitInteger(21, 4)).toEqual([5, 5, 5, 6]);
   });
 });
