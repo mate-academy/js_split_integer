2627ef4d115d7644782d510a4646613395a859f9
Alina Tymchenko <alina.timchenko01@gmail.com>
2021-06-30 13:22:48 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -8,12 +8,32 @@ describe(`Function 'splitInteger':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(typeof splitInteger()).toBe('object');
   });
 
   it(`should return an array with 'num' if parts = 1`, () => {
-
+    expect(splitInteger(8, 1)).toStrictEqual([8]);
   });
-  
+
   // write more tests here
+
+  it(`the parts of the 'num' should be as close as possible`, () => {
+    expect(splitInteger(17, 4)).toStrictEqual([4, 4, 4, 5]);
+  });
+
+  it(`the integers in the result array should be ordered ASC`, () => {
+    expect(splitInteger(32, 6)).toStrictEqual([5, 5, 5, 5, 6, 6]);
+  });
+
+  it(`should return an empty array if num = 3, parts = 'string'`, () => {
+    expect(splitInteger(3, 'string')).toStrictEqual([]);
+  });
+
+  it(`should return an empty array if num = 'string', parts = 5`, () => {
+    expect(splitInteger('string', 5)).toStrictEqual([]);
+  });
+
+  it(`should return an empty array if num = 'string', parts = 'string'`, () => {
+    expect(splitInteger('string', 'string')).toStrictEqual([]);
+  });
 });
