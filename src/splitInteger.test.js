f808eee3ca8a1237a9e0976bfb5aec9563e751c4
VladyslavHonchar <gonchar.momoh@gmail.com>
2021-06-30 19:14:04 +0300

    chidori

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -8,12 +8,26 @@ describe(`Function 'splitInteger':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(splitInteger()).toBeInstanceOf(Array);
   });
 
   it(`should return an array with 'num' if parts = 1`, () => {
-
+    expect(splitInteger(3, 1)).toEqual([3]);
   });
-  
-  // write more tests here
+
+  it(`should return an array of integers`, () => {
+    const res = (splitInteger(20, 3));
+
+    for (const item of res) {
+      expect(typeof item).toBe('number');
+    }
+  });
+
+  it(`should return even parts if 'num % parts = 0'`, () => {
+    expect(splitInteger(25, 5)).toEqual([5, 5, 5, 5, 5]);
+  });
+
+  it(`should return array in the ascending order`, () => {
+    expect(splitInteger(10, 4)).toEqual([2, 2, 3, 3]);
+  });
 });
