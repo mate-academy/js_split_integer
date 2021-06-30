b1da48564ef3ad5b2c8c6e9b0a525da2f82bf67b
varopn <varopn@gmail.com>
2021-06-30 12:12:21 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -8,12 +8,34 @@ describe(`Function 'splitInteger':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(splitInteger()).toBeInstanceOf(Array);
   });
 
   it(`should return an array with 'num' if parts = 1`, () => {
-
+    expect(splitInteger(10, 1)).toEqual([10]);
   });
-  
-  // write more tests here
+
+  it(`should return array with length is equal to the parts parametr`, () => {
+    expect(splitInteger(10, 10).length).toEqual(10);
+  });
+
+  it(`should return array in the ascending order`, () => {
+    expect(splitInteger(50, 3)).toEqual([16, 17, 17]);
+  });
+
+  it(`should return [5, 5] if num = 10, parts = 2`, () => {
+    expect(splitInteger(10, 2)).toEqual([5, 5]);
+  });
+
+  it(`should return [10, 10, 10] if num = 30, parts = 3`, () => {
+    expect(splitInteger(30, 3)).toEqual([10, 10, 10]);
+  });
+
+  it(`should return [59, 59, 59, 59, 60, 60] if num = 356, parts = 6`, () => {
+    expect(splitInteger(356, 6)).toEqual([59, 59, 59, 59, 60, 60]);
+  });
+
+  it(`should return [31881, 31882, 31882] if num = 95645, parts = 3`, () => {
+    expect(splitInteger(95645, 3)).toEqual([31881, 31882, 31882]);
+  });
 });
