9ecc9c5acc2de22ec4343dc393988b4d716be07b
XMODER-ua <sanya22secret@gmail.com>
2021-06-30 15:41:05 +0300

    work is done

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -8,12 +8,26 @@ describe(`Function 'splitInteger':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(splitInteger()).toBeInstanceOf(Array);
   });
 
-  it(`should return an array with 'num' if parts = 1`, () => {
-
+  it(`should return an array with 'num' if 'parts' = 1`, () => {
+    expect(splitInteger(4, 1)).toEqual([4]);
   });
-  
-  // write more tests here
+
+  it(`should return an array with 'num' = 15, 'parts' = 4`, () => {
+    expect(splitInteger(15, 4)).toEqual([3, 4, 4, 4]);
+  });
+
+  it(`should return an array of length 5 if 'parts' = 5`, () => {
+    expect(splitInteger(25, 5)).toEqual([5, 5, 5, 5, 5]);
+  });
+
+  it(`should return an array with 'num' as close as possible`, () => {
+    expect(splitInteger(7, 3)).toEqual([2, 2, 3]);
+  });
+
+  it(`should return an array with 'num' in ascending order`, () => {
+    expect(splitInteger(21, 4)).toEqual([5, 5, 5, 6]);
+  });
 });
