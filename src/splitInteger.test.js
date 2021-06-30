b4570119b091db257b4c0082d5e6daea8268b1e6
Olena <olenazubar@gmail.com>
2021-06-30 15:23:53 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -8,12 +8,22 @@ describe(`Function 'splitInteger':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(splitInteger()).toBeInstanceOf(Array);
   });
 
   it(`should return an array with 'num' if parts = 1`, () => {
-
+    expect(splitInteger(10, 1)).toEqual([10]);
   });
-  
-  // write more tests here
+
+  it(`should return[10, 10, 10] if num = 30, parts = 3`, () => {
+    expect(splitInteger(30, 3)).toEqual([10, 10, 10]);
+  });
+
+  it(`should return[20,20,20] if num = 60, parts = 3`, () => {
+    expect(splitInteger(60, 3)).toEqual([20, 20, 20]);
+  });
+
+  it(`should return 5 elements with num = 10, parts = 5`, () => {
+    expect(splitInteger(10, 5).length).toEqual(5);
+  });
 });
