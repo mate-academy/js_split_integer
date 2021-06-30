14b1b75c1041b52cce0e7599f227aae7aa86f2d0
LinaDagaieva <Lina.yashchenko96@gmail.com>
2021-06-30 14:54:02 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -8,12 +8,27 @@ describe(`Function 'splitInteger':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(splitInteger(10, 3)).toBeInstanceOf(Array);
   });
 
   it(`should return an array with 'num' if parts = 1`, () => {
-
+    expect(splitInteger(8, 1)).toEqual([8]);
   });
-  
-  // write more tests here
+
+  it(`should return min difference between 'num'`, () => {
+    expect(splitInteger(8, 3)).toEqual([2, 3, 3]);
+  });
+
+  it(`should return array in ascending order`, () => {
+    expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+  });
+
+  it(`should return array [5, 5, 5, 5, 6, 6] 
+  with num = 32 if parts = 6`, () => {
+    expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+  });
+
+  it(`should return array [3, 3] with num = 6 if parts = 2`, () => {
+    expect(splitInteger(6, 2)).toEqual([3, 3]);
+  });
 });
