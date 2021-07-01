bcafbb7135b6856867cb51b1abd2dc1afe97a676
Anastasiya-Levkevych <anastasiyalevkevych@gmail.com>
2021-07-01 15:12:53 +0300

    JS Split Integer

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,19 +1,21 @@
-'use strict';
+});
 
-describe(`Function 'splitInteger':`, () => {
-  const splitInteger = require('./splitInteger');
+it(`should return an array`, () => {
 
-  it(`should be declared`, () => {
-    expect(splitInteger).toBeInstanceOf(Function);
-  });
+  expect(splitInteger()).toBeInstanceOf(Array);
+});
 
-  it(`should return an array`, () => {
+it(`should return an array with 'num' if parts = 1`, () => {
+  expect(splitInteger(5, 1)).toEqual([5]);
+});
 
-  });
+it(`should return an array: [5, 5, 5, 5, 6] if number = 26`, () => {
+  expect(splitInteger(26, 5)).toEqual([5, 5, 5, 5, 6]);
+});
 
-  it(`should return an array with 'num' if parts = 1`, () => {
+it(`should return an array in ascending order`, () => {
+  expect(splitInteger(26, 5)).toEqual([5, 5, 5, 5, 6]);
+});
 
-  });
-  
-  // write more tests here
+// write more tests here
 });
