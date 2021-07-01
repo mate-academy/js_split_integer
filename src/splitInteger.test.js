4da41b1a56a114a4b4f658c2ec96db1f63aee0f6
Anastasiya-Levkevych <anastasiyalevkevych@gmail.com>
2021-07-01 15:55:47 +0300

    JS Split Integer

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -5,15 +5,19 @@ it(`should return an array`, () => {
 });
 
 it(`should return an array with 'num' if parts = 1`, () => {
-  expect(splitInteger(5, 1)).toEqual([5]);
+  expect(splitInteger(10, 1)).toEqual([10]);
 });
 
-it(`should return an array: [5, 5, 5, 5, 6] if number = 26`, () => {
-  expect(splitInteger(26, 5)).toEqual([5, 5, 5, 5, 6]);
+it(`should return array with length equal to the parts parametr`, () => {
+  expect(splitInteger(1, 1).length).toEqual(1);
 });
 
-it(`should return an array in ascending order`, () => {
-  expect(splitInteger(26, 5)).toEqual([5, 5, 5, 5, 6]);
+it(`should return array in the ascending order`, () => {
+  expect(splitInteger(61, 3)).toEqual([20, 20, 21]);
+});
+
+it(`should return [4, 4, 4, 5] if num = 17, parts =4`, () => {
+  expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
 });
 
 });
