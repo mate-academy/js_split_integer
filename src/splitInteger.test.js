7cf11a29e489c614229cbe3a59e18395a4ad5146
Dana Oliynyk <dana.victorivna23@gmail.com>
2021-07-01 13:30:11 +0300

    Fixes

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -15,29 +15,23 @@ describe(`Function 'splitInteger':`, () => {
     expect(splitInteger(8, 1)).toEqual([8]);
   });
 
-  it(`should return an array with 'num' if parts = 5`, () => {
+  it(`should return an array with 5 elements`, () => {
     expect(splitInteger(32, 5).length).toBe(5);
   });
 
   it(`should return an integer array`, () => {
-    const result = splitInteger(17, 3); // [5, 6, 6]
+    const result = (splitInteger(17, 3));// [5, 6, 6]
 
-    expect(Number.isInteger(result[0])).toBe(true);
-    expect(Number.isInteger(result[1])).toBe(true);
-    expect(Number.isInteger(result[2])).toBe(true);
+    for (const item of result) {
+      expect(typeof item).toBe('number');
+    }
   });
 
   it(`should return an array as close as possible`, () => {
-    const result = splitInteger(10, 3); // [3, 3, 4]
-
-    expect(result[1] - result[0]).toBeLessThanOrEqual(1);
-    expect(result[2] - result[1]).toBeLessThanOrEqual(1);
+    expect(splitInteger(10, 3)).toEqual([3, 3, 4]);
   });
 
   it(`should return an array in the ascending order`, () => {
-    const result = splitInteger(10, 3); // [3, 3, 4]
-
-    expect(result[0]).toBeLessThanOrEqual(result[1]);
-    expect(result[1]).toBeLessThanOrEqual(result[2]);
+    expect(splitInteger(10, 3)).toEqual([3, 3, 4]);
   });
 });
