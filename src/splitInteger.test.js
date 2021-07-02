eb214c1b8adfea3ab55987558a58d6dae4934758
OlhaZubko <110scof@gmail.com>
2021-07-03 00:49:20 +0300

    homework_1

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,23 +1,23 @@
 'use strict';
 
 describe(`Function 'splitInteger':`, () => {
-const splitInteger = require('./splitInteger');
+  const splitInteger = require('./splitInteger');
 
-it(`should be declared`, () => {
-expect(splitInteger).toBeInstanceOf(Function);
-});
+  it(`should be declared`, () => {
+    expect(splitInteger).toBeInstanceOf(Function);
+  });
 
-it(`should return an array`, () => {
-expect(splitInteger()).toBeInstanceOf(Array);
-});
+  it(`should return an array`, () => {
+    expect(splitInteger()).toBeInstanceOf(Array);
+  });
 
-it(`should return an array with 'num' if parts = 1`, () => {
-expect(splitInteger(8, 1))
-.toEqual([8]);
-});
+  it(`should return an array with 'num' if parts = 1`, () => {
+    expect(splitInteger(8, 1))
+      .toEqual([8]);
+  });
 
-it(`should return an array if 'num' = 66, 'parts' = '3`, () => {
-expect(splitInteger(66, 3))
-.toEqual([22, 22, 22]);
-});
+  it(`should return an array if 'num' = 66, 'parts' = '3`, () => {
+    expect(splitInteger(66, 3))
+      .toEqual([22, 22, 22]);
+  });
 });
