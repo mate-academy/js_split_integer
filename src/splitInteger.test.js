45baa548becba7081e2048adea504977ffd4fcd7
Anastasiya-Levkevych <anastasiyalevkevych@gmail.com>
2021-07-04 23:54:33 +0300

    SplitIntiger Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -11,9 +11,24 @@ describe(`Function 'splitInteger':`, () => {
 
   });
 
-  it(`should return an array with 'num' if parts = 1`, () => {
-
+  it(`should return an integer array`, () => {
+    expect(splitInteger()).toBeInstanceOf(Array);
   });
-  
+
+  it(`should return an array with 'num' if parts = 1`, () => {
+    expect(splitInteger(5, 1)).toEqual([5]);
+  });
+
+  it(`should return an array [3, 3]) if num = 6, parts = 2`, () => {
+    expect(splitInteger(6, 2)).toEqual([3, 3]);
+  });
+
+  it(`should return an array [4, 4, 4, 5] if num = 17, parts = 4`, () => {
+    expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
+  });
+
+  it(`should return an array [5, 5, 5, 5, 6, 6] if num =32, parts = 6`, () => {
+    expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+  });
   // write more tests here
 });
