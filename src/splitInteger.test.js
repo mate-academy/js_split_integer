4bc184a7df7c3197db2d8dfd0dcf1af13f07d52c
Andrii <andriibrykailo@gmail.com>
2021-07-05 19:01:47 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -18,4 +18,30 @@ describe(`Function 'splitInteger':`, () => {
     expect(splitInteger(expectNum, 1)).toEqual([3]);
   });
   // write more tests here
+
+  it(`should return an array of integers`, () => {
+    const res = (splitInteger(20, 3));
+
+    for (const check of res) {
+      expect(typeof check).toBe('number');
+    }
+  });
+
+  it(`should return an array which length is equal to 'parts' value`, () => {
+    expect(splitInteger(20, 3).length).toBe(3);
+  });
+
+  it(`should return an array which contain as close parts of 'num' as possible`,
+    () => {
+      expect(splitInteger(20, 9)).toEqual([2, 2, 2, 2, 2, 2, 2, 3, 3]);
+    });
+
+  it(`should return an array consisting of integers in the ascending order`,
+    () => {
+      const res = (splitInteger(20, 3));
+
+      for (let check = 1; check < res.length; check++) {
+        expect(res[check]).toBeGreaterThanOrEqual(res[check - 1]);
+      }
+    });
 });
