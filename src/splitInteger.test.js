0fce946934f3a082ba972cd1327420c5b280e9e1
Andrii <andriibrykailo@gmail.com>
2021-07-05 18:49:13 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -7,13 +7,15 @@ describe(`Function 'splitInteger':`, () => {
     expect(splitInteger).toBeInstanceOf(Function);
   });
 
-  it(`should return an array`, () => {
-
+  it(`should retur an array`, () => {
+    expect(splitInteger(8, 4))
+      .toBeInstanceOf(Array);
   });
 
   it(`should return an array with 'num' if parts = 1`, () => {
+    const expectNum = 3;
 
+    expect(splitInteger(expectNum, 1)).toEqual([3]);
   });
-  
   // write more tests here
 });
