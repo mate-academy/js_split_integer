3a5396e1f3df55c7e1e9b93b31ecf1d72e28460d
karinababii <karino4ka201351@gmail.com>
2021-10-11 17:49:50 +0300

    Fixed solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -17,19 +17,11 @@ describe(`Function 'splitInteger':`, () => {
     expect(splitInteger(5, 1)).toEqual([5]);
   });
 
-  it(`should return an empty array if parts = 0`, () => {
-    expect(splitInteger(2, 0)).toEqual([]);
-  });
-
   it(`should return an array with integers in the ascending order`, () => {
     expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
   });
 
   it(`should return as close as possible parts of numbers`, () => {
     expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
-  });
-
-  it(`shold return an array if num = -10`, () => {
-    expect(splitInteger(-10, 5)).toEqual([-2, -2, -2, -2, -2]);
   });
 });
