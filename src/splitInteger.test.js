5760d2b272ffceb56593f295c839a4ecb0f0ea0a
Nikita Zhukov <zhukovna1812@gmail.com>
2021-10-11 16:43:32 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -18,9 +18,6 @@ describe(`Function 'splitInteger':`, () => {
   });
 
   // write more tests here
-  it(`should work with negativ 'num'`, () => {
-    expect(splitInteger(-4, 2)).toEqual([-2, -2]);
-  });
 
   it(`should return an empty array if the input is ()`, () => {
     expect(splitInteger()).toEqual([]);
@@ -28,14 +25,6 @@ describe(`Function 'splitInteger':`, () => {
 
   it(`should return an empty array if the input is only 'num'`, () => {
     expect(splitInteger(1)).toEqual([]);
-  });
-
-  it(`should return an empty array if parts = 0`, () => {
-    expect(splitInteger(4, 0)).toEqual([]);
-  });
-
-  it(`should return an empty array if parts is negativ`, () => {
-    expect(splitInteger(4, -2)).toEqual([]);
   });
 
   it(`should return an array [3, 3]]`, () => {
