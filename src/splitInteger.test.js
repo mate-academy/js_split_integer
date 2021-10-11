a71a3524318b405e96a8ceebb1727ea61c61e6d2
Exelleron <exelleron1@gmail.com>
2021-10-11 15:10:45 +0300

    'Solution'

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -8,12 +8,24 @@ describe(`Function 'splitInteger':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(splitInteger(3, 3)).toBeInstanceOf(Array);
   });
 
   it(`should return an array with 'num' if parts = 1`, () => {
-
+    expect(splitInteger(2, 1)).toEqual([2]);
   });
-  
+
   // write more tests here
+
+  it(`should return [4] when inputs is 8, 2`, () => {
+    expect(splitInteger(8, 2)).toEqual([4, 4]);
+  });
+
+  it(`should return empty array when inputs is empty`, () => {
+    expect(splitInteger()).toEqual([]);
+  });
+
+  it(`should return [0] when inputs is 0`, () => {
+    expect(splitInteger(0, 0)).toEqual([]);
+  });
 });
