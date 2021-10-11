c958a5ad2fe1e81bbc2b74cab023f8f625eb6442
Bilinska_Iryna <bilinska.iryna.02@gmail.com>
2021-10-11 16:27:02 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -8,12 +8,26 @@ describe(`Function 'splitInteger':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(splitInteger(65, 5)).toBeInstanceOf(Array);
   });
 
   it(`should return an array with 'num' if parts = 1`, () => {
-
+    expect(splitInteger(33, 1)).toEqual([33]);
   });
-  
-  // write more tests here
+
+  it(`should return an array with 'num' if parts = 1`, () => {
+    expect(splitInteger(25, 5)).toEqual([5, 5, 5, 5, 5]);
+  });
+
+  it(`the parts of the num should be as close as possible`, () => {
+    expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+  });
+
+  it(`integers in the result array should be in ascending order;`, () => {
+    expect(splitInteger(73, 5)).toEqual([14, 14, 15, 15, 15]);
+  });
+
+  it(`the input to your function will always be valid`, () => {
+    expect(splitInteger(324, 5)).toEqual([64, 65, 65, 65, 65]);
+  });
 });
