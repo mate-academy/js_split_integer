13de4ca45919291825f389ca23b606d4d94949ef
Elena <o.orelqa@gmail.com>
2021-10-11 17:14:48 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -15,15 +15,11 @@ describe(`Function 'splitInteger':`, () => {
     expect(splitInteger(8, 1)).toEqual([8]);
   });
 
-  it(`should return empty array if 'num' = 0`, () => {
-    expect(splitInteger(0, 0)).toEqual([]);
-  });
-
-  it(`should return an array  [3, 3] if 'num' = 6, parts = 2`, () => {
+  it(`should return an array [3, 3] if 'num' = 6, parts = 2`, () => {
     expect(splitInteger(6, 2)).toEqual([3, 3]);
   });
 
-  it(`should return an array  [4, 4, 4, 5] if 'num' = 17, parts = 4`, () => {
+  it(`should return an array [4, 4, 4, 5] if 'num' = 17, parts = 4`, () => {
     expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
   });
 });
