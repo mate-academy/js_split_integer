d61100adb67b6357d8ed65abae0b35613f53dc85
Anton Zabolotny <toxober@gmail.com>
2021-10-11 15:18:07 +0300

    solution v1

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -8,12 +8,33 @@ describe(`Function 'splitInteger':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(splitInteger(3, 3)).toBeInstanceOf(Array);
   });
 
   it(`should return an array with 'num' if parts = 1`, () => {
-
+    expect(splitInteger(3, 1)).toStrictEqual([3]);
   });
-  
-  // write more tests here
+
+  it(`should return an array with 'num' if parts = 1`, () => {
+    expect(splitInteger(3, 1)).toStrictEqual([3]);
+  });
+
+  it(`there are parts elements (for example, if parts = 5, 
+    the num would be splitted into 5 integers)`, () => {
+    expect(splitInteger(0, 5)).toStrictEqual([0, 0, 0, 0, 0]);
+  });
+
+  it(`the integers in the result array should be 
+  in the ascending order`, () => {
+    expect(splitInteger(27, 13))
+      .toStrictEqual([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3]);
+  });
+
+  it(`should return result type 'Object'`, () => {
+    expect(typeof splitInteger(3, 1)).toBe('object');
+  });
+
+  it(`should work with negative 'num'`, () => {
+    expect(splitInteger(-15, 3)).toEqual([-5, -5, -5]);
+  });
 });
