d4eb63f54af7307228def6c86a29e5017d72a1ed
zapchik <foramazon157@gmail.com>
2021-10-11 17:32:58 +0300

    Fixed Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -7,7 +7,7 @@ describe(`Function 'splitInteger':`, () => {
     expect(splitInteger).toBeInstanceOf(Function);
   });
 
-  it(`should return an array`, () => {
+  it(`returns an array if function arguments are passed`, () => {
     expect(splitInteger(8, 1)).toBeInstanceOf(Array);
   });
 
@@ -15,8 +15,8 @@ describe(`Function 'splitInteger':`, () => {
     expect(splitInteger(8, 1)).toEqual([8]);
   });
 
-  // write more tests here
-  it(`there are parts elements `, () => {
+  it(`there are parts elements (for example, if parts = 5,
+     the num would be splitted into 5 integers) `, () => {
     expect(splitInteger(26, 5)).toEqual([5, 5, 5, 5, 6]);
   });
 
@@ -26,5 +26,9 @@ describe(`Function 'splitInteger':`, () => {
 
   it(`Numbers must be in ascending order`, () => {
     expect(splitInteger(38, 6)).toEqual([6, 6, 6, 6, 7, 7]);
+  });
+
+  it(`"num" multiple of "parts"`, () => {
+    expect(splitInteger(36, 6)).toEqual([6, 6, 6, 6, 6, 6]);
   });
 });
