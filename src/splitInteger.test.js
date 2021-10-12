4fb1244d2823e5db56c86a4f6214ba7847dd44ed
Bohdo <sooqiable@gmail.com>
2021-10-12 20:29:55 +0300

    Minor fix!

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -11,7 +11,7 @@ describe(`Function 'splitInteger':`, () => {
     expect(splitInteger()).toBeInstanceOf(Array);
   });
 
-  it(`should return an array length of equal the part`, () => {
+  it(`should return an array with length equal to 'parts'`, () => {
     expect(splitInteger(10, 2)).toEqual([5, 5]);
   });
 
@@ -19,15 +19,11 @@ describe(`Function 'splitInteger':`, () => {
     expect(splitInteger(8, 1)).toEqual([8]);
   });
 
-  it(`should return an array with 'num' if parts = 2`, () => {
-    expect(splitInteger(6, 2)).toEqual([3, 3]);
+  it(`should return integers as close to each other as possible`, () => {
+    expect(splitInteger(8, 3)).toEqual([2, 3, 3]);
   });
 
   it(`should return an array in ascending order`, () => {
     expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
-  });
-
-  it(`should return an integer array`, () => {
-    expect(splitInteger(2, 1)).toEqual([2]);
   });
 });
