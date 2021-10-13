daa16ae2a0a589645798d0e47308e41616860f0b
Eugene Chornyi <winheisbox@gmail.com>
2021-10-13 23:09:49 +0300

    HomeWorkBlack

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -8,12 +8,19 @@ describe(`Function 'splitInteger':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(typeof splitInteger(17, 4)).toEqual('object');
   });
 
   it(`should return an array with 'num' if parts = 1`, () => {
-
+    expect(splitInteger(8, 1)).toEqual([8]);
   });
-  
+
   // write more tests here
+  it(`'part' must be equal to the length of the array 'result'`, () => {
+    expect(splitInteger(17, 4)).toHaveLength(4);
+  });
+
+  it(`should return values to an array in ascending order`, () => {
+    expect(splitInteger(37, 5)).toEqual([7, 7, 7, 8, 8]);
+  });
 });
