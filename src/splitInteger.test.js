f3c354908234419e8aafa979a0b090b57cc77005
Igor Havryliak <kachkamala@gmail.com>
2022-01-05 19:10:37 +0200

    Solution 1

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,17 +1,19 @@
+// eslint-disable-next-line strict
 const splitInteger = require('./splitInteger');
 
-test(`should split a number into equal parts if a value is divisible by a numberOfParts`, () => {
-
+it(`should split a number into equal parts if a 
+value is divisible by a numberOfParts`, () => {
+  expect(splitInteger(4, 2)).toEqual([2, 2]);
 });
 
-test(`should return a part equals to a value when splitting into 1 part`, () => {
-
+it(`should return a part equals to a value when splitting into 1 part`, () => {
+  expect(splitInteger(1, 1)).toEqual([1]);
 });
 
-test('should sort parts ascending if they are not equal', () => {
-
+it('should sort parts ascending if they are not equal', () => {
+  expect(splitInteger(9, 8)).toEqual([1, 1, 1, 1, 1, 1, 1, 2]);
 });
 
-test('should add zeros if value < numberOfParts', () => {
-
+it('should add zeros if value < numberOfParts', () => {
+  expect(splitInteger(3, 4)).toEqual([0, 1, 1, 1]);
 });
