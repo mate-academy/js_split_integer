7684ef218f284e739d2584eb7bd742ecb74f379d
krolich500grn <edu.izaytsev@gmail.com>
2022-07-01 15:25:36 +0300

    feat: unit tests for splitInteger function

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,17 +1,29 @@
+'use strict';
+
 const splitInteger = require('./splitInteger');
 
-test(`should split a number into equal parts if a value is divisible by a numberOfParts`, () => {
+test(`should split a number into equal
+parts if a value is divisible by a numberOfParts`, () => {
+  const parts = splitInteger(8, 4);
 
+  expect(parts).toEqual([2, 2, 2, 2]);
 });
 
-test(`should return a part equals to a value when splitting into 1 part`, () => {
+test(`should return a part equals
+to a value when splitting into 1 part`, () => {
+  const parts = splitInteger(10, 1);
 
+  expect(parts).toEqual([10]);
 });
 
 test('should sort parts ascending if they are not equal', () => {
+  const parts = splitInteger(15, 4);
 
+  expect(parts).toEqual([3, 4, 4, 4]);
 });
 
 test('should add zeros if value < numberOfParts', () => {
+  const parts = splitInteger(3, 5);
 
+  expect(parts).toEqual([0, 0, 1, 1, 1]);
 });
