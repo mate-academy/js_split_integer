4a9f419ad49fce4de86ac0e64854091199cc5c0f
lena <lenapatrashok@gmail.com>
2022-08-20 10:06:11 +0300

    Task

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,17 +1,27 @@
-const splitInteger = require('./splitInteger');
+'use strict';
 
-test(`should split a number into equal parts if a value is divisible by a numberOfParts`, () => {
+describe(`Function 'splitInteger'`, () => {
+  const splitInteger = require('./splitInteger');
 
-});
+  it(`should split a number into equal parts if a value is divisible by 
+  a numberOfParts`, () => {
+    expect(splitInteger(10, 5))
+      .toStrictEqual([2, 2, 2, 2, 2]);
+  });
 
-test(`should return a part equals to a value when splitting into 1 part`, () => {
+  it(`should return a part equals to a value
+   when splitting into 1 part`, () => {
+    expect(splitInteger(9, 1))
+      .toStrictEqual([9]);
+  });
 
-});
+  it('should sort parts ascending if they are not equal', () => {
+    expect(splitInteger(7, 3))
+      .toStrictEqual([2, 2, 3]);
+  });
 
-test('should sort parts ascending if they are not equal', () => {
-
-});
-
-test('should add zeros if value < numberOfParts', () => {
-
+  it('should add zeros if value < numberOfParts', () => {
+    expect(splitInteger(3, 5))
+      .toStrictEqual([0, 0, 1, 1, 1]);
+  });
 });
