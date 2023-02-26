5a5ac202ebe8e14a6533445725c44f93bde7c5bb
Haidadin Ihor <garikbarik1@gmail.com>
2023-02-26 18:24:56 +0100

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -19,3 +19,4 @@ test('should sort parts ascending if they are not equal', () => {
 test('should add zeros if value < numberOfParts', () => {
   expect(splitInteger(1, 2)).toEqual([0, 1]);
 });
+//Done
