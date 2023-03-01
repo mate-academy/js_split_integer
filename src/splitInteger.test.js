e54f80c4eb634cbe60944ba0cb9dc760b643a0c3
Maryna Oznobishyna <111009638+Oznobishina@users.noreply.github.com>
2023-03-01 08:48:09 +0200

    01.03.2023

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -13,10 +13,7 @@ test(`should return a part equals to a value
 });
 
 test('should return an array containing integer elements', () => {
-  expect(splitInteger(8, 1)).toEqual([8]);
-  expect(splitInteger(6, 2)).toEqual([3, 3]);
   expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
-  expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
 });
 
 test('should add zeros if value < numberOfParts', () => {
@@ -31,4 +28,12 @@ test('should return an object', () => {
 test('should be declared', () => {
   expect(splitInteger)
     .toBeInstanceOf(Function);
+});
+
+test('should sort parts ascending if they are not equal', () => {
+  const goal = splitInteger(17, 4);
+
+  for (let i = 0; i < 3; i++) {
+    expect(goal[i] <= goal[i + 1]).toEqual(true);
+  }
 });
