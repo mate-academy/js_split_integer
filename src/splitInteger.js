fd2f6b4e28a360b9b84d66e61fc8a54266429bd0
Olga Zubko <melichka8550@gmail.com>
2023-03-29 14:26:36 +0300

    Revert "Tests added" This reverts commit 61f8cf9363ee2275e4684c5026a9ff2e371bb2db.

--- src/splitInteger.js
+++ src/splitInteger.js
@@ -4,20 +4,34 @@
  *
  * @returns {number[]}
  */
-'use strict';
+  'use strict';
 
-function splitInteger(value, numberOfParts) {
-  const parts = [];
-  let rest = value;
+  function splitInteger(value, numberOfParts) {
+    const parts = [];
+    let rest = value;
 
-  for (let partsLeft = numberOfParts; partsLeft > 0; partsLeft--) {
-    const part = Math.floor(rest / partsLeft);
+    for (let partsLeft = numberOfParts; partsLeft > 0; partsLeft--) {
+      const part = Math.floor(rest / partsLeft);
 
-    parts.push(part);
-    rest -= part;
+      parts.push(part);
+      rest -= part;
+    }
+
+    return parts;
   }
 
-  return parts;
-}
+  module.exports = splitInteger;
 
-module.exports = splitInteger;
+  test('The difference between the max and min <= 1', () => {
+    const result = splitInteger(10, 3);
+    const max = Math.max(...result);
+    const min = Math.min(...result);
+
+    expect(max - min).toBeLessThanOrEqual(1);
+  });
+
+  test('The array should be sorted ascending', () => {
+    const result = splitInteger(10, 3);
+
+    expect(result).toEqual(result.sort((a, b) => a - b));
+  });
