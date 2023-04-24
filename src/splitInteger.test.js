43b28ca6a2ff6783f04130bb8910b1784c723be5
Oleksandr_Hundertailo <oleksandrhundertailo@gmail.com>
2023-04-24 22:28:47 +0200

    Add changes according to mentor comments

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -5,9 +5,8 @@ const splitInteger = require('./splitInteger');
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
   const goals = splitInteger(17, 4);
-  const equalParts = goals.slice(0, 3);
 
-  expect(equalParts).toEqual([4, 4, 4]);
+  expect(goals).toEqual([4, 4, 4, 5]);
 });
 
 test(`should return a part equals to a value
@@ -23,7 +22,7 @@ test('should sort parts ascending if they are not equal', () => {
   const sort = goals.sort((a, b) => a - b);
 
   expect(goals)
-    .toBe(sort);
+    .toEqual(sort);
 });
 
 test('should add zeros if value < numberOfParts', () => {
