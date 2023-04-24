86e7bcbfc8e224ef25dd6b17569ad094c3bc8f44
Anastasiia Zheliaskova <a.zheliaskova3@gmail.com>
2023-04-24 14:49:30 +0300

    Solution

--- src/splitInteger.js
+++ src/splitInteger.js
@@ -1,9 +1,12 @@
+'use strict';
+
 /**
  * @param {number} value
  * @param {number} numberOfParts
  *
  * @returns {number[]}
  */
+
 function splitInteger(value, numberOfParts) {
   const parts = [];
   let rest = value;
