2b338402fcd99cf540494f0a4d18c90597cc2397
Ruslan Vrazovskyi <dnkons@gmail.com>
2023-06-16 21:26:47 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -19,3 +19,10 @@ test('should sort parts ascending if they are not equal', () => {
 test('should add zeros if value < numberOfParts', () => {
 
 });
+
+test('should split into equal parts if divisible', () => {
+  expect(splitInteger(8, 1)).toEqual([8]);
+  expect(splitInteger(6, 2)).toEqual([3, 3]);
+  expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
+  expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+});
