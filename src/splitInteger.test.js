de8ad5e8d3c7e2ab76034078f477ccf671fc2a8c
Vladyslav Lysenko <86077101+Vlad110200@users.noreply.github.com>
2023-08-10 22:04:35 +0300

    Update splitInteger.test.js

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -11,15 +11,15 @@ test(`should split a number into equal parts
 test(`should return a part equals to a value
   when splitting into 1 part`, () => {
     expect(splitInteger(8, 1))
-      .toEgual([8]);
+      .toEqual([8]);
 });
 
 test('should sort parts ascending if they are not equal', () => {
     expect(splitInteger(17, 4))
-      .toEgual([4, 4, 4, 5]);
+      .toEqual([4, 4, 4, 5]);
 });
 
 test('should add zeros if value < numberOfParts', () => {
     expect(splitInteger(4, 5))
-      .toEgual(0, 1, 1, 1, 1);
+      .toEqual(0, 1, 1, 1, 1);
 });
