9f975c44da74a665c92e03c9fcee5a5b1f219c37
H0N <honyy.qa@gmail.com>
2023-08-11 19:43:48 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -2,20 +2,36 @@
 
 const splitInteger = require('./splitInteger');
 
-test(`should split a number into equal parts
-  if a value is divisible by a numberOfParts`, () => {
+describe('#splitInteger', () => {
+  test(`should split a number into equal parts
+    if a value is divisible by a numberOfParts`, () => {
+    const mockedValue = 12;
+    const mockedNumberOfParts = 3;
+    const splitedInteger = splitInteger(mockedValue, mockedNumberOfParts);
 
-});
+    expect(splitedInteger).toEqual([4, 4, 4]);
+  });
 
-test(`should return a part equals to a value
-  when splitting into 1 part`, () => {
+  test(`should return a part equals to a
+  value when splitting into 1 part`, () => {
+    const mockedValue = 10;
+    const mockedNumberOfParts = 1;
 
-});
+    expect(splitInteger(mockedValue, mockedNumberOfParts)).toEqual([10]);
+  });
 
-test('should sort parts ascending if they are not equal', () => {
+  test('should sort parts ascending if they are not equal', () => {
+    const mockedValue = 19;
+    const mockedNumberOfParts = 3;
 
-});
+    expect(splitInteger(mockedValue, mockedNumberOfParts)).toEqual([6, 6, 7]);
+  });
 
-test('should add zeros if value < numberOfParts', () => {
+  test('should add zeros if value < numberOfParts', () => {
+    const mockedValue = 2;
+    const mockedNumberOfParts = 5;
 
+    expect(splitInteger(mockedValue, mockedNumberOfParts))
+      .toEqual([0, 0, 0, 1, 1]);
+  });
 });
