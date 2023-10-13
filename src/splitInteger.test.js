3b628879d65b7634e223b93b7387d0ef4f995f12
Olesia Semen <semenolesa01@gmail.com>
2023-10-13 13:33:44 +0300

    Testing

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,21 +1,25 @@
 'use strict';
 
-const splitInteger = require('./splitInteger');
+const { splitInteger } = require('./your-code-file'); // Replace 'your-code-file' with the actual file path
 
-test(`should split a number into equal parts
-  if a value is divisible by a numberOfParts`, () => {
+describe('splitInteger', () => {
+  it('should return [8] when splitting 8 into 1 part', () => {
+    const result = splitInteger(8, 1);
+    expect(result).to.deep.equal([8]);
+  });
 
-});
+  it('should return [3, 3] when splitting 6 into 2 parts', () => {
+    const result = splitInteger(6, 2);
+    expect(result).to.deep.equal([3, 3]);
+  });
 
-test(`should return a part equals to a value
-  when splitting into 1 part`, () => {
+  it('should return [4, 4, 4, 5] when splitting 17 into 4 parts', () => {
+    const result = splitInteger(17, 4);
+    expect(result).to.deep.equal([4, 4, 4, 5]);
+  });
 
-});
-
-test('should sort parts ascending if they are not equal', () => {
-
-});
-
-test('should add zeros if value < numberOfParts', () => {
-
+  it('should return [5, 5, 5, 5, 6, 6] when splitting 32 into 6 parts', () => {
+    const result = splitInteger(32, 6);
+    expect(result).to.deep.equal([5, 5, 5, 5, 6, 6]);
+  });
 });
