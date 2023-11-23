daecf3388af1fab0573ee5cd908f52b252ccc6d1
yuliia <yuliya.o.siromakha@gmail.com>
2023-11-23 20:32:55 +0200

    Add test suite

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -5,17 +5,21 @@ const splitInteger = require('./splitInteger');
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
 
-});
+    expect(splitInteger(10, 5)).toBe(5); 
+  });
 
 test(`should return a part equals to a value
   when splitting into 1 part`, () => {
 
+    expect(splitInteger(2, 1)).toBe(2); 
 });
 
 test('should sort parts ascending if they are not equal', () => {
 
+    expect(sortedParts).toEqual([...parts].sort());
 });
 
 test('should add zeros if value < numberOfParts', () => {
 
+    expect(splitInteger(0, 0)).toBe(0);
 });
