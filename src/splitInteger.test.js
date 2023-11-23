67ed5782ffb890da3f774a86519744f7563a21ce
Artem Nikolaichuk <tompsoq@gmail.com>
2023-11-23 15:34:09 +0200

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -19,3 +19,13 @@ test('should sort parts ascending if they are not equal', () => {
 test('should add zeros if value < numberOfParts', () => {
 
 });
+
+test(`the difference between the max and min number
+ in the array should be <= 1`, () => {
+  expect(Math.max(splitInteger(17, 4)) - Math.min(splitInteger(17, 4)) || 0)
+    .toBeLessThanOrEqual(1);
+});
+
+test(`the array should be sorted ascending (from lowest to highest`, () => {
+  expect(splitInteger(13, 4)).toEqual([3, 3, 3, 4]);
+});
