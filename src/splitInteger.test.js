e18e2c861258fc347f1bf638fc8768c4e2811bcc
Maksym <tarasovmv90@gmail.com>
2023-11-27 17:14:06 +0200

    split this integer

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -5,29 +5,71 @@ const splitInteger = require('./splitInteger');
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
   expect(splitInteger(10, 5)).toEqual([2, 2, 2, 2, 2]);
+});
+
+test(`should split a number into equal parts
+  if a value is divisible by a numberOfParts`, () => {
   expect(splitInteger(15, 3)).toEqual([5, 5, 5]);
+});
+
+test(`should split a number into equal parts
+  if a value is divisible by a numberOfParts`, () => {
   expect(splitInteger(20, 5)).toEqual([4, 4, 4, 4, 4]);
+});
+
+test(`should split a number into equal parts
+  if a value is divisible by a numberOfParts`, () => {
   expect(splitInteger(27, 3)).toEqual([9, 9, 9]);
 });
 
 test(`should return a part equals to a value
   when splitting into 1 part`, () => {
   expect(splitInteger(8, 1)).toEqual([8]);
+});
+
+test(`should return a part equals to a value
+  when splitting into 1 part`, () => {
   expect(splitInteger(20, 1)).toEqual([20]);
+});
+
+test(`should return a part equals to a value
+  when splitting into 1 part`, () => {
   expect(splitInteger(15, 1)).toEqual([15]);
+});
+
+test(`should return a part equals to a value
+  when splitting into 1 part`, () => {
   expect(splitInteger(39, 1)).toEqual([39]);
 });
 
 test('should sort parts ascending if they are not equal', () => {
   expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
+});
+
+test('should sort parts ascending if they are not equal', () => {
   expect(splitInteger(25, 6)).toEqual([4, 4, 4, 4, 4, 5]);
+});
+
+test('should sort parts ascending if they are not equal', () => {
   expect(splitInteger(12, 5)).toEqual([2, 2, 2, 3, 3]);
+});
+
+test('should sort parts ascending if they are not equal', () => {
   expect(splitInteger(7, 3)).toEqual([2, 2, 3]);
 });
 
 test('should add zeros if value < numberOfParts', () => {
   expect(splitInteger(5, 8)).toEqual([0, 0, 0, 1, 1, 1, 1, 1]);
+});
+
+test('should add zeros if value < numberOfParts', () => {
   expect(splitInteger(10, 12)).toEqual([0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+});
+
+test('should add zeros if value < numberOfParts', () => {
   expect(splitInteger(3, 5)).toEqual([0, 0, 1, 1, 1]);
+});
+
+test('should add zeros if value < numberOfParts', () => {
   expect(splitInteger(7, 9)).toEqual([0, 0, 1, 1, 1, 1, 1, 1, 1]);
 });
