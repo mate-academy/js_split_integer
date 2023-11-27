18bb1d701c7003cb1381d4d1a8f3c6508e503a42
drabykDi <148562996+drabykDi@users.noreply.github.com>
2023-11-27 14:29:35 +0200

    Update splitInteger.test.js

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -4,7 +4,7 @@ const splitInteger = require('./splitInteger');
 
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
-  expect(splitInteger(8, 1)).toEqual([8]);
+  expect(splitInteger(20, 4)).toEqual([5, 5, 5, 5]);
 });
 
 test(`should return a part equals to a value
@@ -17,17 +17,17 @@ test('should sort parts ascending if they are not equal', () => {
 });
 
 test('should add zeros if value < numberOfParts', () => {
-  expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+  expect(splitInteger(4, 5)).toEqual([0, 1, 1, 1, 1]);
 });
 
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
-  expect(splitInteger(10, 1)).toEqual([10]);
+  expect(splitInteger(10, 2)).toEqual([5, 5]);
 });
 
 test(`should return a part equals to a value
   when splitting into 1 part`, () => {
-  expect(splitInteger(8, 2)).toEqual([4, 4]);
+  expect(splitInteger(8, 1)).toEqual([8]);
 });
 
 test('should sort parts ascending if they are not equal', () => {
@@ -35,5 +35,5 @@ test('should sort parts ascending if they are not equal', () => {
 });
 
 test('should add zeros if value < numberOfParts', () => {
-  expect(splitInteger(43, 6)).toEqual([7, 7, 7, 7, 7, 8]);
+  expect(splitInteger(3, 6)).toEqual([0, 0, 0, 1, 1, 1]);
 });
