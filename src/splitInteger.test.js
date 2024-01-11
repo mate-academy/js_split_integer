328701fefcb13933d329f0f4be302aae69319366
Olga Belmega <olga.belmega@gmail.com>
2024-01-11 14:03:45 +0200

    Update

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -4,18 +4,18 @@ const splitInteger = require('./splitInteger');
 
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
-
+    expect(splitInteger(9, 3)).toEqual([3, 3, 3]);
 });
 
 test(`should return a part equals to a value
   when splitting into 1 part`, () => {
-
+    expect(splitInteger(6, 1)).toEqual([6]);
 });
 
 test('should sort parts ascending if they are not equal', () => {
-
+  expect(splitInteger(10, 4)).toEqual([2, 2, 3, 3]);
 });
 
 test('should add zeros if value < numberOfParts', () => {
-
+  expect(splitInteger(1, 6)).toEqual([0, 1]);
 });
