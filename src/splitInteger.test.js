f70ae525575d6101aa506abd4fb8e488eb968686
OlgaBelmega <143718672+OlgaBelmega@users.noreply.github.com>
2024-01-12 09:39:25 +0200

    New update

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -17,5 +17,5 @@ test('should sort parts ascending if they are not equal', () => {
 });
 
 test('should add zeros if value < numberOfParts', () => {
-  expect(splitInteger(1, 6)).toEqual([0, 1]);
+  expect(splitInteger(1, 6)).toEqual([0, 0, 0, 0, 0, 1]);
 });
