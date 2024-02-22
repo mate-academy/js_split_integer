c716a31c42177439503a95d68094ef3f8d7b11ca
Вероніка <veronikadzul@gmail.com>
2024-02-22 08:44:51 +0200

    Fixed

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -6,8 +6,7 @@ test(`should split a number into equal parts
 if a value is divisible by a numberOfParts`, () => {
   expect(splitInteger(8, 1)).toEqual([8]);
   expect(splitInteger(6, 2)).toEqual([3, 3]);
-  expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
-  expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+  expect(splitInteger(10, 2)).toEqual([5, 5]);
 });
 
 test(`should return a part equals to a value
@@ -23,5 +22,5 @@ test('should sort parts ascending if they are not equal', () => {
 });
 
 test('should add zeros if value < numberOfParts', () => {
-  expect(splitInteger(3, 5)).toEqual([0, 0, 1, 1, 1]);
+  expect(splitInteger(6, 7)).toEqual([0, 1, 1, 1, 1, 1, 1]);
 });
