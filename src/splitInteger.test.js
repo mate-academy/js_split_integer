fce93c78d71848962a1a2f1a3710679285ce8610
IvanGrabovsky <ivan.grabovsky.ua@gmail.com>
2024-03-14 10:24:18 +0200

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -4,18 +4,26 @@ const splitInteger = require('./splitInteger');
 
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
+  const result = splitInteger(972, 3);
 
+  expect(result).toEqual([324, 324, 324]);
 });
 
 test(`should return a part equals to a value
   when splitting into 1 part`, () => {
+  const result = splitInteger(97, 1);
 
+  expect(result).toEqual([97]);
 });
 
 test('should sort parts ascending if they are not equal', () => {
+  const result = splitInteger(1754, 4);
 
+  expect(result).toEqual([438, 438, 439, 439]);
 });
 
 test('should add zeros if value < numberOfParts', () => {
+  const result = splitInteger(4, 6);
 
+  expect(result).toEqual([0, 0, 1, 1, 1, 1]);
 });
