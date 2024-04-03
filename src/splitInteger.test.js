9f73640048d08cae701288d061d9aee26ed41cbf
MarkelKoy <demagog1231@gmail.com>
2024-04-03 12:53:54 +0300

    God loves you

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -2,20 +2,28 @@
 
 const splitInteger = require('./splitInteger');
 
-test(`should split a number into equal parts
-  if a value is divisible by a numberOfParts`, () => {
-
+test('Split integer into 1 part', () => {
+  expect(splitInteger(8, 1)).toEqual([8]);
 });
 
-test(`should return a part equals to a value
-  when splitting into 1 part`, () => {
-
+test('Split integer into 2 parts', () => {
+  expect(splitInteger(6, 2)).toEqual([3, 3]);
 });
 
-test('should sort parts ascending if they are not equal', () => {
+test('Split integer into 4 parts', () => {
+  const result = splitInteger(17, 4);
 
+  expect(result).toHaveLength(4);
+  expect(result.reduce((acc, val) => acc + val)).toBe(17);
+  expect(Math.max(...result) - Math.min(...result)).toBeLessThanOrEqual(1);
+  expect(result).toEqual(result.sort((a, b) => a - b));
 });
 
-test('should add zeros if value < numberOfParts', () => {
+test('Split integer into 6 parts', () => {
+  const result = splitInteger(32, 6);
 
+  expect(result).toHaveLength(6);
+  expect(result.reduce((acc, val) => acc + val)).toBe(32);
+  expect(Math.max(...result) - Math.min(...result)).toBeLessThanOrEqual(1);
+  expect(result).toEqual(result.sort((a, b) => a - b));
 });
