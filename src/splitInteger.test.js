bb975c50619cfc5e564f1cb5f2913f256794a1d3
Yurii Mohylnytskyi <yuriistarlife@gmail.com>
2024-04-03 18:26:00 +0300

    Tests: 8 passed; 8 total

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -17,5 +17,5 @@ test('should sort parts ascending if they are not equal', () => {
 });
 
 test('should add zeros if value < numberOfParts', () => {
-  expect(splitInteger(1, 2)).toEqual([0, 0]);
+  expect(splitInteger(1, 2)).toEqual([0, 1]);
 });
