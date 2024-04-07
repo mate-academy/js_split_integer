6e3730244477d96a1ee96b250baba0f5cf8f33eb
Your Name <your_email@whatever.com>
2024-04-08 00:21:44 +0300

    add tests

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -4,36 +4,27 @@ const splitInteger = require('./splitInteger');
 
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
-  const result = splitInteger(10, 5);
-
-  expect(result.length).toBe(5);
-  expect(Math.max(...result) - Math.min(...result)).toBeLessThanOrEqual(1);
-  expect(result).toEqual([2, 2, 2, 2, 2]);
+  expect(splitInteger(8, 1)).toEqual([8]);
+  expect(splitInteger(6, 2)).toEqual([3, 3]);
+  expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
+  expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
 });
 
 test(`should return a part equals to a value
   when splitting into 1 part`, () => {
-  const result = splitInteger(8, 1);
-
-  expect(result.length).toBe(1);
-  expect(result).toEqual([8]);
+  expect(splitInteger(8, 1)).toEqual([8]);
+  expect(splitInteger(6, 1)).toEqual([6]);
+  expect(splitInteger(17, 1)).toEqual([17]);
+  expect(splitInteger(32, 1)).toEqual([32]);
 });
 
 test('should sort parts ascending if they are not equal', () => {
-  const result = splitInteger(15, 3);
-
-  expect(result.length).toBe(3);
-  expect(Math.max(...result) - Math.min(...result)).toBeLessThanOrEqual(1);
-  expect(result).toEqual([5, 5, 5]);
+  expect(splitInteger(8, 4)).toEqual([2, 2, 2, 2]);
+  expect(splitInteger(15, 5)).toEqual([3, 3, 3, 3, 3]);
+  expect(splitInteger(20, 3)).toEqual([6, 7, 7]);
 });
 
 test('should add zeros if value < numberOfParts', () => {
-  const result = splitInteger(4, 6);
-  const zerosCount = result.filter(num => num === 0).length;
-  const onesCount = result.filter(num => num === 1).length;
-
-  expect(result.length).toBe(6);
-  expect(zerosCount).toBe(2);
-  expect(onesCount).toBe(4);
-  expect(Math.max(...result) - Math.min(...result)).toBeLessThanOrEqual(1);
+  expect(splitInteger(1, 6)).toStrictEqual([0, 0, 0, 0, 0, 1]);
+  expect(splitInteger(7, 10)).toEqual([0, 0, 0, 1, 1, 1, 1, 1, 1, 1]);
 });
