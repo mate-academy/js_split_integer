912d3ae288abcbc8429ec05e2dabea807bead4b0
gajdziormax@gmail.com <gajdziormax@gmail.com>
2024-04-17 15:06:32 +0200

    tests

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,21 +1,31 @@
 'use strict';
 
+const assert = require('assert');
+
 const splitInteger = require('./splitInteger');
 
-test(`should split a number into equal parts
-  if a value is divisible by a numberOfParts`, () => {
+function test(message, callback) {
+  try {
+    callback();
+    console.log(`${message} - Passed`);
+  } catch (error) {
+    console.log(`${message} - Failed`);
+    console.error(error);
+  }
+}
 
+test(`should split a number into equal parts if a value is divisible by a numberOfParts`, () => {
+  assert.deepEqual(splitInteger(10, 2), [5, 5]);
 });
 
-test(`should return a part equals to a value
-  when splitting into 1 part`, () => {
-
+test(`should return a part equals to a value when splitting into 1 part`, () => {
+  assert.deepEqual(splitInteger(7, 1), [7]);
 });
 
 test('should sort parts ascending if they are not equal', () => {
-
+  assert.deepEqual(splitInteger(21, 6), [3, 3, 3, 3, 4, 5]);
 });
 
 test('should add zeros if value < numberOfParts', () => {
-
+  assert.deepEqual(splitInteger(3, 5), [0, 0, 1, 1, 1]);
 });
