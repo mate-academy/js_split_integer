ba91d6869bc9a9946a429d35e2b74e51d33890d7
Ihnat-Zinkevich <ignatzinkevch@gmail.com>
2024-04-23 16:10:35 +0200

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -2,23 +2,45 @@
 
 const splitInteger = require('./splitInteger');
 
-test(`should split a number into equal parts
-  if a value is divisible by a numberOfParts`, () => {
-  expect(splitInteger(10, 2)).toEqual([5, 5]);
+// test(`should split a number into equal parts
+//   if a value is divisible by a numberOfParts`, () => {
+//   expect(splitInteger(10, 2)).toEqual([5, 5]);
+// });
+
+// test(`should return a part equals to a value
+//   when splitting into 1 part`, () => {
+//   expect(splitInteger(8, 1)).toEqual([8]);
+// });
+
+// test('should sort parts ascending if they are not equal', () => {
+//   const result = splitInteger(10, 4);
+//   const expected = [2, 2, 3, 3];
+
+//   expect(result).toEqual(expected);
+// });
+
+// test('should add zeros if value < numberOfParts', () => {
+//   expect(splitInteger(1, 2)).toEqual([0, 1]);
+// });
+
+test(`the difference between the max and min number
+ in the array should be <= 1`, () => {
+  const result = splitInteger(10, 7);
+  const equalOrLessOne = result[0] - result[result.length - 1] <= 1;
+
+  expect(equalOrLessOne).toEqual(true);
 });
 
-test(`should return a part equals to a value
-  when splitting into 1 part`, () => {
-  expect(splitInteger(8, 1)).toEqual([7]);
-});
+test('the array should be sorted ascending (from lowest to highest)', () => {
+  const result = splitInteger(10, 3);
+  let isSorted = true;
 
-test('should sort parts ascending if they are not equal', () => {
-  const result = splitInteger(10, 4);
-  const expected = [4, 4, 2];
+  for (let i = 0; i < result.length - 1; i++) {
+    if (result[i] > result[i + 1]) {
+      isSorted = false;
+      break;
+    }
+  }
 
-  expect(result).toEqual(expected);
-});
-
-test('should add zeros if value < numberOfParts', () => {
-  expect(splitInteger(1, 2)).toEqual([0, 0]);
+  expect(isSorted).toBe(true);
 });
