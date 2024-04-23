e83b1e0958f419672254e843c8816816a67f687f
Ihnat-Zinkevich <ignatzinkevch@gmail.com>
2024-04-23 13:13:29 +0200

    jest 2

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -9,13 +9,16 @@ test(`should split a number into equal parts
 
 test(`should return a part equals to a value
   when splitting into 1 part`, () => {
-
+  expect(splitInteger(8, 1)).toEqual([7]);
 });
 
 test('should sort parts ascending if they are not equal', () => {
+  const result = splitInteger(10, 4);
+  const expected = [4, 4, 2];
 
+  expect(result).toEqual(expected);
 });
 
 test('should add zeros if value < numberOfParts', () => {
-
+  expect(splitInteger(1, 2)).toEqual([0, 0]);
 });
