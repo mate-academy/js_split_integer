09c7ff7c261aa637d9940bcbe3bbc2e9d948e6e0
Ihnat-Zinkevich <161659312+Ihnat-Zinkevich@users.noreply.github.com>
2024-05-04 01:13:17 +0200

    Fixed solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -2,26 +2,26 @@
 
 const splitInteger = require('./splitInteger');
 
-// test(`should split a number into equal parts
-//   if a value is divisible by a numberOfParts`, () => {
-//   expect(splitInteger(10, 2)).toEqual([5, 5]);
-// });
+test(`should split a number into equal parts
+  if a value is divisible by a numberOfParts`, () => {
+  expect(splitInteger(10, 2)).toEqual([5, 5]);
+});
 
-// test(`should return a part equals to a value
-//   when splitting into 1 part`, () => {
-//   expect(splitInteger(8, 1)).toEqual([8]);
-// });
+test(`should return a part equals to a value
+  when splitting into 1 part`, () => {
+  expect(splitInteger(8, 1)).toEqual([8]);
+});
 
-// test('should sort parts ascending if they are not equal', () => {
-//   const result = splitInteger(10, 4);
-//   const expected = [2, 2, 3, 3];
+test('should sort parts ascending if they are not equal', () => {
+  const result = splitInteger(10, 4);
+  const expected = [2, 2, 3, 3];
 
-//   expect(result).toEqual(expected);
-// });
+  expect(result).toEqual(expected);
+});
 
-// test('should add zeros if value < numberOfParts', () => {
-//   expect(splitInteger(1, 2)).toEqual([0, 1]);
-// });
+test('should add zeros if value < numberOfParts', () => {
+  expect(splitInteger(1, 2)).toEqual([0, 1]);
+});
 
 test(`the difference between the max and min number
  in the array should be <= 1`, () => {
