7078e6806dcc99d6994ea39d2f1502c6d458637f
Oleksandr Bielov <alehandro.bel96@gmail.com>
2024-06-04 15:15:58 +0300

    My solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,21 +1,38 @@
 'use strict';
 
-const splitInteger = require('./splitInteger');
+const { splitInteger } = require('./splitInteger');
 
-test(`should split a number into equal parts
-  if a value is divisible by a numberOfParts`, () => {
+describe(`'splitInteger' should`, () => {
 
-});
+  test(`return an array`, () => {
+    const result = splitInteger(12, 5);
 
-test(`should return a part equals to a value
-  when splitting into 1 part`, () => {
+    expect(result).toBeInstanceOf(Array);
+  });
 
-});
+  test(`split a number into equal parts
+    if a value is divisible by a numberOfParts`, () => {
+    const result = splitInteger(12, 5);
 
-test('should sort parts ascending if they are not equal', () => {
+    expect(result).toEqual([2, 2, 2, 3, 3]);
+  });
 
-});
+  test(`return a part equals to a value
+    when splitting into 1 part`, () => {
+    const result = splitInteger(5, 1);
 
-test('should add zeros if value < numberOfParts', () => {
+    expect(result).toEqual([5]);
+  });
 
+  test('sort parts ascending if they are not equal', () => {
+    const result = splitInteger(13, 5);
+
+    expect(result).toEqual([2, 2, 3, 3, 3]);
+  });
+
+  test('add zeros if value < numberOfParts', () => {
+    const result = splitInteger(2, 5);
+
+    expect(result).toEqual([0, 0, 0, 1, 1]);
+  });
 });
