b510dc897ef91c8138757f588a966f05b71e3987
Ilya <pulcho91@gmail.com>
2024-06-11 20:15:31 +0300

    Solution

--- src/splitInteger.js
+++ src/splitInteger.js
@@ -1,21 +1,20 @@
 // splitInteger.js
 function splitInteger(value, numberOfParts) {
-  const result = [];  // Создаем пустой массив в который будем добавлять части числа
-  const baseValue = Math.floor(value / numberOfParts);   // Вычисляем базовое значение для каждой части округляя вниз
-  const remainder = value % numberOfParts;   // Вычисляем остаток, который остается после равного деления
+  const result = [];
+  const baseValue = Math.floor(value / numberOfParts);
+  const remainder = value % numberOfParts;
 
-  // Заполняем массив частями числа
   for (let i = 0; i < numberOfParts; i++) {
-    // Если индекс меньше остатка добавляем базовое значение + 1
+
     if (i < remainder) {
       result.push(baseValue + 1);
     } else {
-      // Иначе добавляем только базовое значение
+
       result.push(baseValue);
     }
   }
-// Возвращаем массив, отсортированный по возрастанию
+
   return result.sort((a, b) => a - b);
 }
-// Экспорт функции
+
 module.exports = splitInteger;
