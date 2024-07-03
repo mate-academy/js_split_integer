adbf725abe8ea12a3513cc9cf4478e1881abb304
“Klaudia <klaudia@atomicweb.design>
2024-07-03 19:21:18 +0100

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,21 +1,41 @@
+// src/splitInteger.test.js
+
 'use strict';
 
 const splitInteger = require('./splitInteger');
 
-test(`should split a number into equal parts
-  if a value is divisible by a numberOfParts`, () => {
+test('should split into equal parts if divisible', () => {
+  const value = 12;
+  const numberOfParts = 4;
+  const result = splitInteger(value, numberOfParts);
 
+  expect(result).toEqual([3, 3, 3, 3]);
 });
 
-test(`should return a part equals to a value
-  when splitting into 1 part`, () => {
+test('should return value when parts = 1', () => {
+  const value = 15;
+  const numberOfParts = 1;
+  const result = splitInteger(value, numberOfParts);
 
+  expect(result).toEqual([15]);
 });
 
-test('should sort parts ascending if they are not equal', () => {
+test('should sort parts in ascending order', () => {
+  const value = 18;
+  const numberOfParts = 3;
+  const result = splitInteger(value, numberOfParts);
 
+  expect(result).toEqual([6, 6, 6]);
 });
 
-test('should add zeros if value < numberOfParts', () => {
+test('should add zeros if value < parts', () => {
+  const value = 5;
+  const numberOfParts = 8;
+  const result = splitInteger(value, numberOfParts);
 
+  expect(result.length).toBe(numberOfParts);
+
+  const sum = result.reduce((acc, curr) => acc + curr, 0);
+
+  expect(sum).toBe(value);
 });
