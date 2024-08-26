2cc4ad07cd33d3425d7e1c34ef5be5ba184c5258
Anna <kozlovskaya.h@gmail.com>
2024-08-26 13:00:54 +0300

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -4,8 +4,8 @@ const splitInteger = require('./splitInteger');
 
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
-  const number = 150;
-  const numberOfParts = 3;
+  let number = 150;
+  let numberOfParts = 3;
   const expectedOutput = [50, 50, 50];
   const result = splitInteger(number, numberOfParts);
 
@@ -14,7 +14,7 @@ test(`should split a number into equal parts
 
 test(`should return a part equals to a value
   when splitting into 1 part`, () => {
-  const valueToSplit = 150;
+  let valueToSplit = 150;
   const numberOfParts = 1;
   const result = splitInteger(valueToSplit, numberOfParts);
 
@@ -22,8 +22,8 @@ test(`should return a part equals to a value
 });
 
 test('should return parts sorted in asc order if they are not equal', () => {
-  const value = 10;
-  const numberOfParts = 3;
+  let value = 10;
+  let numberOfParts = 3;
   const result = splitInteger(value, numberOfParts);
   const isSorted = result.every((part, index) => index
     === 0 || part >= result[index - 1]);
@@ -34,7 +34,7 @@ test('should return parts sorted in asc order if they are not equal', () => {
 });
 
 test('should add zeros if value < numberOfParts', () => {
-  const result = splitInteger(1, 5);
+  let result = splitInteger(1, 5);
 
   expect(result).toEqual([0, 0, 0, 0, 1]);
 });
