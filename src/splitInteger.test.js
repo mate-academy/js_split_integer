9f416dcbb3a611214a5a2bbba2ca57ab5211d351
Mariana Khrustina <khrystinamariana@gmail.com>
2024-09-05 16:43:52 +0400

    Added test to check if zeros are correctly placed in the array when value is less than numberOfParts

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -3,19 +3,13 @@
 const splitInteger = require('./splitInteger');
 
 describe('splitInteger', () => {
-  test(
-    'should return [8] when value is 8 and numberOfParts is 1',
-    () => {
-      expect(splitInteger(8, 1)).toEqual([8]);
-    }
-  );
+  test('should return [8] when value is 8 and numberOfParts is 1', () => {
+    expect(splitInteger(8, 1)).toEqual([8]);
+  });
 
-  test(
-    'should return [3, 3] when value is 6 and numberOfParts is 2',
-    () => {
-      expect(splitInteger(6, 2)).toEqual([3, 3]);
-    }
-  );
+  test('should return [3, 3] when value is 6 and numberOfParts is 2', () => {
+    expect(splitInteger(6, 2)).toEqual([3, 3]);
+  });
 
   test(
     'should return [4, 4, 4, 5] when value is 17 and numberOfParts is 4',
@@ -28,6 +22,15 @@ describe('splitInteger', () => {
     'should return [5, 5, 5, 5, 6, 6] when value is 32 and numberOfParts is 6',
     () => {
       expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+    }
+  );
+
+  test(
+    'should return array with zeros when value is less than numberOfParts',
+    () => {
+      expect(splitInteger(2, 5)).toEqual([0, 0, 0, 1, 1]);
+      expect(splitInteger(3, 6)).toEqual([0, 0, 0, 1, 1, 1]);
+      expect(splitInteger(0, 4)).toEqual([0, 0, 0, 0]);
     }
   );
 });
