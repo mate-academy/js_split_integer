ae3e9b98caaa8ba56daf47582220b54358c76114
Daria <kravetsdarja@icloud.com>
2024-09-05 13:02:02 +0300

    Solution

--- src/splitInteger.js
+++ src/splitInteger.js
@@ -5,17 +5,21 @@
  * @returns {number[]}
  */
 function splitInteger(value, numberOfParts) {
-  const parts = [];
-  let rest = value;
-
-  for (let partsLeft = numberOfParts; partsLeft > 0; partsLeft--) {
-    const part = Math.floor(rest / partsLeft);
-
-    parts.push(part);
-    rest -= part;
-  }
-
-  return parts;
+  const basePart = Math.floor(value / numberOfParts);
+  const remainder = value % numberOfParts; 
+  const result = Array(numberOfParts).fill(basePart);
+  for (let i = 0; i < remainder; i++) {
+    result[i]++;
 }
 
-module.exports = splitInteger;
+return result.sort((a, b) => a - b);
+}
+function runTests() {
+  console.log(splitInteger(10, 2));
+  console.log(splitInteger(10, 3));
+  console.log(splitInteger(5, 5));
+  console.log(splitInteger(9, 4)); 
+  console.log(splitInteger(8, 3)); 
+}
+
+runTests();
