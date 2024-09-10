905889f684b3cb4f919f040c7de258c2fe0fe07c
gazebo88fire <gazebo88fire@gmail.com>
2024-09-10 17:28:25 +0300

    Solution2

--- src/splitInteger.js
+++ src/splitInteger.js
@@ -1,49 +1,23 @@
 'use strict';
 
+/**
+ * @param {number} value
+ * @param {number} numberOfParts
+ *
+ * @returns {number[]}
+ */
 function splitInteger(value, numberOfParts) {
-  const basePart = Math.floor(value / numberOfParts);
-  const remainder = value % numberOfParts;
-  const result = Array(numberOfParts).fill(basePart);
+  const parts = [];
+  let rest = value;
 
-  for (let i = 0; i < remainder; i++) {
-    result[i]++;
+  for (let partsLeft = numberOfParts; partsLeft > 0; partsLeft--) {
+    const part = Math.floor(rest / partsLeft);
+
+    parts.push(part);
+    rest -= part;
   }
 
-  return result.sort((a, b) => a - b);
+  return parts;
 }
 
-describe('splitInteger', () => {
-  test('should return an array with one element when numberOfParts is 1',
-    () => {
-      expect(splitInteger(8, 1)).toEqual([8]);
-    });
-
-  test('should return equal parts when value can be evenly divided', () => {
-    expect(splitInteger(6, 2)).toEqual([3, 3]);
-  });
-
-  test('should return parts with a'
-    + 'difference of 1 when value cannot be evenly divided',
-  () => {
-    expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
-  });
-
-  test('should handle larger values and multiple parts', () => {
-    expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
-  });
-
-  test('should return sorted array in ascending order', () => {
-    const result = splitInteger(29, 5);
-
-    expect(result).toEqual([5, 5, 6, 6, 7]);
-    expect(result).toBeSorted();
-  });
-
-  test('should handle case where value is smaller than numberOfParts', () => {
-    expect(splitInteger(3, 5)).toEqual([0, 0, 0, 1, 2]);
-  });
-
-  test('should handle case where value equals numberOfParts', () => {
-    expect(splitInteger(4, 4)).toEqual([1, 1, 1, 1]);
-  });
-});
+module.exports = splitInteger;
--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,19 +1,33 @@
 'use strict';
 
-test(`should split a number into equal parts
-  if a value is divisible by a numberOfParts`, () => {
+const splitInteger = require('./splitInteger');
 
-});
+describe('splitInteger', () => {
+  test('should return [8] when value is 8 and numberOfParts is 1', () => {
+    expect(splitInteger(8, 1)).toEqual([8]);
+  });
 
-test(`should return a part equals to a value
-  when splitting into 1 part`, () => {
+  test('should return [3, 3] when value is 6 and numberOfParts is 2', () => {
+    expect(splitInteger(6, 2)).toEqual([3, 3]);
+  });
 
-});
+  test('should return [4, 4, 4, 5] when value is 17'
+    + 'and numberOfParts is 4', () => {
+    expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
+  });
 
-test('should sort parts ascending if they are not equal', () => {
+  test('should return [5, 5, 5, 5, 6, 6] when value is 32'
+    + 'and numberOfParts is 6', () => {
+    expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+  });
 
-});
+  test('should return [1, 1, 1, 1, 1] when value is 5'
+    + 'and numberOfParts is 5', () => {
+    expect(splitInteger(5, 5)).toEqual([1, 1, 1, 1, 1]);
+  });
 
-test('should add zeros if value < numberOfParts', () => {
-
+  test('should return [0, 0, 1, 1, 1] when value is 3'
+    + 'and numberOfParts is 5', () => {
+    expect(splitInteger(3, 5)).toEqual([0, 0, 1, 1, 1]);
+  });
 });
