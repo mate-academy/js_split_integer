0ffe94b82d7211fdff8e6a671d83c711b5e9701f
Galers <galosaandrew@gmail.com>
2024-12-09 17:22:15 +0200

    test: Change test

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -2,43 +2,34 @@
 
 const splitInteger = require("./splitInteger");
 
-describe(`Split integer`, () => {
-  test(`should split a number into equal parts
+test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
-    expect(splitInteger(8, 1)).toEqual([8]);
-    expect(splitInteger(6, 2)).toEqual([3, 3]);
-    expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
-    expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
-  });
+  expect(splitInteger(8, 1)).toEqual([8]);
+  expect(splitInteger(6, 2)).toEqual([3, 3]);
+  expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
+  expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+});
 
-  test(`should return a part equals to a value
+test(`should return a part equals to a value
   when splitting into 1 part`, () => {
-    const isDifferenceValid = (array) => {
-      if (array.length === 0) {
-        return true;
-      }
+  const result1 = splitInteger(17, 4);
 
-      const max = Math.max(array);
-      const min = Math.min(array);
+  expect(Math.max(...result1) - Math.min(...result1)).toBeLessThanOrEqual(1);
 
-      return max - min <= 1;
-    };
+  const result2 = splitInteger(32, 6);
 
-    expect(isDifferenceValid(splitInteger(17, 4))).toEqual(
-      isDifferenceValid(splitInteger(17, 4))
-    );
-  });
+  expect(Math.max(...result2) - Math.min(...result2)).toBeLessThanOrEqual(1);
+});
 
-  test(`should sort parts ascending if they are not equal`, () => {
-    expect(splitInteger(6, 2)).toStrictEqual(splitInteger(6, 2).sort());
-    expect(splitInteger(17, 4)).toStrictEqual(splitInteger(17, 4).sort());
-    expect(splitInteger(32, 6)).toStrictEqual(splitInteger(32, 6).sort());
-  });
+test(`should sort parts ascending if they are not equal`, () => {
+  const result = splitInteger(29, 6);
 
-  test(`should add zeros if value < numberOfParts`, () => {
-    expect(splitInteger(0, 1)).toEqual([0]);
-    expect(splitInteger(0, 2)).toEqual([0, 0]);
-    expect(splitInteger(0, 4)).toEqual([0, 0, 0, 0]);
-    expect(splitInteger(0, 6)).toEqual([0, 0, 0, 0, 0, 0]);
-  });
+  expect(result).toEqual(result.slice().sort((a, b) => a - b));
+});
+
+test(`should add zeros if value < numberOfParts`, () => {
+  expect(splitInteger(0, 1)).toEqual([0]);
+  expect(splitInteger(0, 2)).toEqual([0, 0]);
+  expect(splitInteger(2, 4)).toEqual([0, 0, 1, 1]);
+  expect(splitInteger(7, 10)).toEqual([0, 0, 0, 1, 1, 1, 1, 1, 1, 1]);
 });
