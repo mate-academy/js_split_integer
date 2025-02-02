c2ab12cbfef62e7618b1bb107a43854e54341afa
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2025-02-02 19:56:08 +0200

    Solution

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,21 +1,47 @@
-'use strict';
+"use strict";
 
-const splitInteger = require('./splitInteger');
+const splitInteger = require("./splitInteger");
 
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
+  const result = splitInteger(6, 2);
 
+  expect(Math.min(...result)).toBe(Math.max(...result));
 });
 
 test(`should return a part equals to a value
   when splitting into 1 part`, () => {
+  const result = splitInteger(8, 1);
 
+  expect(result[0]).toBe(8);
 });
 
-test('should sort parts ascending if they are not equal', () => {
+test("should sort parts ascending if they are not equal", () => {
+  const result = splitInteger(32, 6);
 
+  const sorted = result.sort((num1, num2) => {
+    return num1 - num2;
+  });
+
+  expect(result).toBe(sorted);
 });
 
-test('should add zeros if value < numberOfParts', () => {
+test("should add zeros if value < numberOfParts", () => {
+  const result = splitInteger(2, 4);
+  const expected = [0, 0, 1, 1];
 
+  expect(result).toEqual(expected);
 });
+
+// eslint-disable-next-line no-shadow
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(
+          `actual is not equal expected ${actual} !== ${expected}`
+        );
+      }
+    },
+  };
+}
