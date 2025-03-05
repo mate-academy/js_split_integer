74f9392399362eae4084b86ee1565f360458d7a5
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-03-05 15:41:29 +0100

    splitInteger

--- src/splitInteger.js
+++ src/splitInteger.js
@@ -1,23 +1,15 @@
 'use strict';
 
-/**
- * @param {number} value
- * @param {number} numberOfParts
- *
- * @returns {number[]}
- */
 function splitInteger(value, numberOfParts) {
-  const parts = [];
-  let rest = value;
+  const part = Math.floor(value / numberOfParts);
+  const remainder = value % numberOfParts;
+  const result = Array(numberOfParts).fill(part);
 
-  for (let partsLeft = numberOfParts; partsLeft > 0; partsLeft--) {
-    const part = Math.floor(rest / partsLeft);
-
-    parts.push(part);
-    rest -= part;
+  for (let i = 0; i < remainder; i++) {
+    result[i]++;
   }
 
-  return parts;
+  return result.sort((a, b) => a - b);
 }
 
 module.exports = splitInteger;
