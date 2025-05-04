d989280a3fe009ce46779c78df3a6aa7675689ed
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-05-04 15:16:13 +0200

    solution

--- src/splitInteger.js
+++ src/splitInteger.js
@@ -10,8 +10,8 @@ function splitInteger(value, numberOfParts) {
   const parts = [];
   let rest = value;
 
-  for (let partsLeft = numberOfParts; partsLeft > 0; partsLeft--) {
-    const part = Math.floor(rest / partsLeft);
+  for (let i = numberOfParts; i > 0; i--) {
+    const part = Math.floor(rest / i);
 
     parts.push(part);
     rest -= part;
