d609cd0205d080be3f48d85a07cda0a13a0ebf86
Yurii_Popovskiy <ronondex663@gmail.com>
2025-05-15 11:44:24 +0300

    addSomeChanges

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -7,7 +7,9 @@ test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
   expect(splitInteger(6, 2)).toEqual([3, 3]);
   expect(splitInteger(10, 5)).toEqual([2, 2, 2, 2, 2]);
-  expect(splitInteger(100, 10)).toEqual([10, 10, 10, 10, 10, 10, 10, 10, 10, 10]);
+
+  expect(splitInteger(100, 10)).toEqual([10, 10, 10, 10, 10,
+    10, 10, 10, 10, 10]);
 });
 
 test(`should return a part equals to a value
