2189647e28c89ba9f910b3f7275d1fc0d1452907
Anastasiia_Klymenko <asia.kvlchuk@gmail.com>
2025-05-26 14:22:30 +0300

    Add implementation of splitInteger function

---
+++ splitInteger.js
@@ -0,0 +1,11 @@
+function splitInteger(value, numberOfParts) {
+  const base = Math.floor(value / numberOfParts);
+  const remainder = value % numberOfParts;
+  const result = Array(numberOfParts).fill(base);
+  for (let i = 0; i < remainder; i++) {
+    result[numberOfParts - 1 - i]++;
+  }
+  return result.sort((a, b) => a - b);
+}
+
+module.exports = splitInteger;
