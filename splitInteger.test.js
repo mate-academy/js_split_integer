aaf8d0ea03291492bc18817394d2356bf9f5357e
Maryna Lemishko <munchik4@gmail.com>
2025-05-27 14:22:53 +0300

    Create tests1

---
+++ splitInteger.test.js
@@ -0,0 +1,8 @@
+const splitInteger = require('./splitInteger');
+
+test('splitInteger basic tests', () => {
+  expect(splitInteger(8, 1)).toEqual([8]);
+  expect(splitInteger(6, 2)).toEqual([3, 3]);
+  expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
+  expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+});
