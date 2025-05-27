bc01c6c758902b90afc8fdb7cf3cd472ed939ab3
Shararashik <shararashik04@online.ua>
2025-05-27 10:37:17 +0300

    Write test that should split a number into equal parts   if a value is divisible by a numberOfParts

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -4,7 +4,13 @@ const splitInteger = require('./splitInteger');
 
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
-
+  const value = 12;
+  const numberOfParts = 4;
+  const expected = [3, 3, 3, 3];
+ 
+  const result = splitInteger(value, numberOfParts);
+ 
+  expect(result).toEqual(expected); 
 });
 
 test(`should return a part equals to a value
