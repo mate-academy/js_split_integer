a2d8ec39522d32d8ebb906c7722af82133387a23
Heny <jackbalageru@yahoo.com>
2025-06-25 16:47:17 +0300

    Solution

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-
-'use strict';
-
-const { createServer } = require('./createServer');
-
-createServer().listen(5700, () => {
-  console.log('Server is running on localhost:5700');
-});
