6dfbceda0a5d612241af50cd6a2e1bb54c79a90e
kateliaskov <58365756+KateLis84@users.noreply.github.com>
2025-08-07 21:02:17 +0300

    add new test

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -19,3 +19,7 @@ test('should sort parts ascending if they are not equal', () => {
 test('should add zeros if value < numberOfParts', () => {
   expect(splitInteger(2, 5)).toEqual([0, 0, 0, 1, 1]);
 });
+
+test('should distribute the remainder correctly for larger values', () => {
+  expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+});
