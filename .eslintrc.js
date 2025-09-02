15adb00ae1f19bd73f0a4cb9eaada421d9d84ca1
Mykhailo Hubko <mixelios@gmail.com>
2025-09-02 15:31:34 +0200

    all test completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
-  extends: '@mate-academy/eslint-config',
+  extends: ['@mate-academy/eslint-config', 'plugin:jest/recommended'],
   env: {
     jest: true
   },
   rules: {
-    'no-proto': 0,
-    strict: 0,
-    'max-len': 0,
+    'no-proto': 0
   },
   plugins: ['jest']
 };
