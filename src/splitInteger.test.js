67b8f55f32cd715de1717e9a39be920609602fb6
Mykhailo <kurochkinmy@gmail.com>
2025-11-08 14:54:37 +0100

    feat: add more tests for other cases

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -27,3 +27,19 @@ test('should add zeros if value < numberOfParts', () => {
 
   expect(result).toEqual([0, 1, 1, 1, 1]);
 });
+
+test('splitInteger(6, 2) should return [3, 3]', () => {
+  const result = splitInteger(6, 2);
+  expect(result).toEqual([3, 3]);
+});
+
+test('splitInteger(17, 4) should return [4, 4, 4, 5]', () => {
+  const result = splitInteger(17, 4);
+  expect(result).toEqual([4, 4, 4, 5]);
+});
+
+test('splitInteger(32, 6) should return [5, 5, 5, 5, 6, 6]', () => {
+  const result = splitInteger(32, 6);
+  expect(result).toEqual([5, 5, 5, 5, 6, 6]);
+});
+
