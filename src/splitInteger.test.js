e6f50d5df361c9262c129cf7f9c67acdf0cd5869
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-11-12 11:38:36 +0200

    fixed

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -24,3 +24,19 @@ test("should add zeros if value < numberOfParts", () => {
   expect(splitInteger(2, 4)).toEqual([0, 0, 1, 1]);
   expect(splitInteger(1, 3)).toEqual([0, 0, 1]);
 });
+
+test("splitInteger(8, 1) should return [8]", () => {
+  expect(splitInteger(8, 1)).toEqual([8]);
+});
+
+test("splitInteger(6, 2) should return [3, 3]", () => {
+  expect(splitInteger(6, 2)).toEqual([3, 3]);
+});
+
+test("splitInteger(17, 4) should return [4, 4, 4, 5]", () => {
+  expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
+});
+
+test("splitInteger(8, 1) should return [8]", () => {
+  expect(splitInteger(8, 1)).toEqual([8]);
+});
