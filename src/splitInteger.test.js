b92a290305ec076c9a4ad69128acd197112ba99f
Anna Nikiforova <annanicky90@gmail.com>
2025-11-27 23:52:53 +0400

    Fix the issues

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -11,19 +11,14 @@ test(`should split a number into equal parts
 
 test(`should return a part equals to a value
   when splitting into 1 part`, () => {
-  const parts = splitInteger(8, 1);
-
-  expect(parts).toEqual([8]);
+  expect(splitInteger(8, 1)).toEqual([8]);
 });
 
 test('should sort parts ascending if they are not equal', () => {
-  const parts = splitInteger(17, 4);
-
-  expect(parts).toEqual([4, 4, 4, 5]);
+  expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
+  expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
 });
 
 test('should add zeros if value < numberOfParts', () => {
-  const parts = splitInteger(3, 6);
-
-  expect(parts).toEqual([0, 0, 0, 1, 1, 1]);
+  expect(splitInteger(3, 6)).toEqual([0, 0, 0, 1, 1, 1]);
 });
