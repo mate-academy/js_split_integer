3d2b461aea5ea684f146fe00bf285d0fde83ef46
melarkkkk <melark@192-168-100-126.lan>
2025-12-27 08:51:29 +0100

    feat: add additional cases to the existing tests

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -4,6 +4,7 @@ const splitInteger = require('./splitInteger');
 
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
+  expect(splitInteger(6, 2)).toEqual([3, 3]);
   expect(splitInteger(8, 1)).toEqual([8]);
   expect(splitInteger(8, 2)).toEqual([4, 4]);
   expect(splitInteger(8, 4)).toEqual([2, 2, 2, 2]);
@@ -18,8 +19,8 @@ test(`should return a part equals to a value
 
 test('should sort parts ascending if they are not equal', () => {
   expect(splitInteger(8, 5)).toEqual([1, 1, 2, 2, 2]);
-  expect(splitInteger(5, 2)).toEqual([2, 3]);
-  expect(splitInteger(10, 3)).toEqual([3, 3, 4]);
+  expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
+  expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
 });
 
 test('should add zeros if value < numberOfParts', () => {
@@ -27,3 +28,4 @@ test('should add zeros if value < numberOfParts', () => {
   expect(splitInteger(1, 2)).toEqual([0, 1]);
   expect(splitInteger(3, 4)).toEqual([0, 1, 1, 1]);
 });
+
