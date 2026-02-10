9d9e08e582538996b2b163482f26e09bff10da70
Nikita Doroshenko <nikitokd05@gmail.com>
2026-02-10 13:01:15 +0200

    Add test from the example

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -16,6 +16,10 @@ test('should sort parts ascending if they are not equal', () => {
   expect(splitInteger(17, 4)).toEqual([4, 4, 4, 5]);
 });
 
+test('should sort parts ascending if they are not equal', () => {
+  expect(splitInteger(32, 6)).toEqual([5, 5, 5, 5, 6, 6]);
+});
+
 test('should add zeros if value < numberOfParts', () => {
   expect(splitInteger(2, 3)).toEqual([0, 1, 1]);
 });
